d6a5fe8d8864f49289b7f36856f5b81b4ec7fceb
Vijay Saran <vijaysaran0707@gmail.com>
2024-02-10 19:24:35 +0530

    Added test suite for testing locally

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require('express')
 const cors = require('cors')
 const { default: mongoose } = require('mongoose')
 const mangoose = require('mongoose')
-const url = 'mongodb://localhost:27017/Employee' 
-const app= express()
-mongoose.connect(url,{useNewUrlParser:true})
+const url = 'mongodb://localhost:27017/Employee'
+const app = express()
+mongoose.connect(url, { useNewUrlParser: true })
 const con = mongoose.connection
-con.on('open',() => {
+con.on('open', () => {
     console.log('Connected............')
 })
 app.use(express.json())
 app.use(cors());
 const employeesRouter = require('./routes/employees')
-app.use('/employees',employeesRouter)
-app.listen(9000,() => {
+app.use('/employees', employeesRouter)
+app.listen(9000, () => {
     console.log("Server is started.......")
 })
+
+module.exports = app;
